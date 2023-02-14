267570ccf716f1d23e2b37ca810ed4d6e660e0fb
snackli17 <snackli17@gmail.com>
2023-02-14 20:26:30 +0200

    v
    v

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,8 +18,8 @@ export class App extends Component {
     name: '',
     number: '',
   };
+  // contacts;
 
-  
   componentDidMount() {
     const contactsFromLocalStorage = localStorage.getItem('contactList');
     const parsedContacts = JSON.parse(contactsFromLocalStorage);
